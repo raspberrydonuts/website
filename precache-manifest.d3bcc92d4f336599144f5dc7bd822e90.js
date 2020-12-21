0b0f8d9af220e7d86a52b4a06c6205739f42bc41
Jared Lim <jaredlim320@gmail.com>
2020-12-21 17:42:10 -0600

    Updates

---
+++ precache-manifest.d3bcc92d4f336599144f5dc7bd822e90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f11c207876cd276cddb6",
+    "url": "/static/css/main.de3f2d53.chunk.css"
+  },
+  {
+    "revision": "f11c207876cd276cddb6",
+    "url": "/static/js/main.f11c2078.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "9ebd47c19ef5a77543f67a5b63e01e79",
+    "url": "/index.html"
+  }
+];
