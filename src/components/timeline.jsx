9a03a565af7274670024a83735e5cb11b1993b22
Jared Lim <jaredlim320@gmail.com>
2020-12-21 17:03:10 -0600

    files

---
+++ src/components/timeline.jsx
@@ -0,0 +1,153 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Began Graduate Studies <span>January 2021</span></h2>
+                        <p>I completed my undergraduate studies for my Bachelor of Science in computer science and began my graduate studies at the University of Minnesota, Twin Cities to obtain my Master of Science in Computer Science. Planning to pursue research in software engineering</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Software Engineering Internship at Target Corporation <span>May 2020-December 2020</span></h2>
+                        <p>This was a remote internship during the COVID-19 pandemic where I was a member of the 3D Outbound team in the supply chain division. Here, I modernized legacy supply chain SQL procedures to Java Spring Boot services. Additionally, I built, deployed, and maintained a centralized search interface with ReactJS that allowed teams to more efficiently track the movement of products in distribution centers. My internship was extended throughout the school semester, and during this time I resolved external data access issues by engineering Kotlin Spring Boot microservices for other teams to access supply chain Oracle databases.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Teaching Assistant at University of Minnesota <span>August 2020-December 2020</span></h2>
+                        <p>I was an undergraduate teaching assistant for the course: CSCI3081W - Program Design and Development under Dr. Mattia Fazzini. I collaborated with teaching staff to administer coursework, evaluate submissions, and analyze class performance in a remote learning environment due to COVID-19.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-2">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Joined WECharity at University of Minnesota <span>January 2020-January 2021</span></h2>
+                        <p>I was appointed Event Chairman of the University of Minnesota's chapter of WECharity. My role was to organize charity events to raise money for specific causes. Our highlight for this year was raising over $2000 for folks in Yemen who were severely affected by the COVID-19 pandemic.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-1">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Joined Girls Who Code at University of Minnesota <span>December 2019-May 2020</span></h2>
+                        <p>I joined as a programming mentor for a nonprofit organization which aims to support and increase the number of women in computer science by equipping young women with the necessary computing skills to pursue 21st century opportunities. </p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Technology Consultant / Application Development Internship at RSM US LLP <span>June 2019-August 2019</span></h2>
+                        <p>At RSM, I was a technology consultant intern on the application development team. Here, I consulted with clients on the design and optimization of controls, solutions, and products utilizing extensive knowledge of information systems. I was introduced to creating solutions for clients rather than for internal use. My first project was developing a ticketing management system using .NET Core full-stack web application framework. Then, I implemented a user interface with Vue.js for the first ever ERP-based Blockchain traceability solution.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Software Developer Internship at COUNTRY Financial <span>May 2018-May 2019</span></h2>
+                        <p>At COUNTRY Financial, I was a technology consultant intern on the Automation and Deployments team. Here, I was introduced to software development using the Dev Ops methodology where I automated and deployed software using Gitlab pipelines to deliver applications and services at high velocity. Our team's main product was a Django web app that receives traffic from Gitlab CI Jobs and performs actions on their behalf. My role in this was implementing various services for this application using Gitlab's API. My internship was extended an entire year, where I programmed Click command-line interfaces for automation tools using Gitab’s API.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Joined UMN Application Development Club <span>January 2018-December 2018</span></h2>
+                        <p>I joined my school's app dev club and I collaborated with developers and researchers to develop an application that assesses students’ competencies to identify particular social and behavioral needs and provides guidance to teachers on how to support their students using Android Studio and Java.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-2">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Joined CoderDojo <span>October 2017-January 2021</span></h2>
+                        <p>I joined an organzation where we mentored youth to code, build a website, create an app or game, and explore technology in an informal, creative, and social environment in a free, volunteer-led, community-based programming club.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-1">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Technology Help Specialist at UMN Office of Information Technology <span>July 2017-June 2020</span></h2>
+                        <p>During most of my undergraduate career, I was a technology help specialist at my school's IT department. For the first two years, I provided technical support for the entire University Community via phone, chat, SMS, email, face-to-­face, and using remote tools. Additionally, I oversaw local and remote computer lab facilities including customer support, security, and hardware/software troubleshooting. In 2019, I was promoted to Desktop Support Specialist where I provided specialized computer and device support in tech support zones. In 2020, I helped facilitate the transition for the entire University staff and faculty to work remotely during the COVID-19 pandemic.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Began Undergraduate Studies <span>August 2017-January 2021</span></h2>
+                        <p>In 2017, I began my undergraduate studies to pursue my Bachelor of Science in computer Science. </p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
