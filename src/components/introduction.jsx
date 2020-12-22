a152d64907b7edc48f1421a47bd77b07a881ac64
Jared Lim <jaredlim320@gmail.com>
2020-12-22 15:11:57 -0600

    update spacing and links

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -2,60 +2,103 @@ import React, { Component } from 'react'
 
 export default class Introduction extends Component {
   render() {
-    return (
-      <div>
-        <section id="colorlib-hero" className="js-fullheight" data-section="home">
-          <div className="flexslider js-fullheight">
-            <ul className="slides">
-              <li style={{backgroundImage: 'url(images/img_bg.jpeg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner js-fullheight">
-                        <div className="desc">
-                          <h1>Hi! <br />I'm Jared</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              <li style={{backgroundImage: 'url(images/img_bg.jpeg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc" >
-                          <h1>I love building<br /> things</h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://github.com/dbarochiya" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li>
-              {/* <li style={{backgroundImage: 'url(images/img_bg.jpeg)'}}>
-                <div className="overlay" />
-                <div className="container-fluid">
-                  <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
-                      <div className="slider-text-inner">
-                        <div className="desc">
-                          <h1>I often <br/>Write ... </h1>
-                          <p><a className="btn btn-primary btn-learn" href="https://dhruvbarochiya.me/" target="_blank" rel="noopener noreferrer">View Blog <i className="icon-book" /></a></p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </li> */}
-            </ul>
-          </div>
-        </section>
-      </div>
-    )
+    return <Intro />
   }
+}
+
+function Intro(props) {
+  const [width, setWidth] = React.useState(window.innerWidth);
+  if (width < 800) {
+    return <MobileIntro />;
+  } else {
+    return <DesktopIntro />;
+  }
+}
+
+function DesktopIntro(props) {
+  return (
+    <div>
+      <section id="colorlib-hero" className="js-fullheight" data-section="home">
+        <div className="flexslider js-fullheight">
+          <ul className="slides">
+            <li style={{backgroundImage: 'url(images/img_bg.jpeg)'}}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner js-fullheight">
+                      <div className="desc">
+                        <h1>Hey, <br />I'm Jared</h1>
+                        <p><a className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li style={{backgroundImage: 'url(images/img_bg.jpeg)'}}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner">
+                      <div className="desc" >
+                        <h1>I love building<br /> things</h1>
+                        <p><a className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts" target="_blank" rel="noopener noreferrer">View Projects</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+function MobileIntro(props) {
+  return (
+    <div>
+      <section id="colorlib-hero" className="js-fullheight" data-section="home">
+        <div className="flexslider js-fullheight">
+          <ul className="slides">
+            <li style={{backgroundImage: 'url(images/img_bg.jpeg)'}}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner js-fullheight">
+                      <div style={{marginTop: '300px'}} className="desc">
+                        <h1 style={{textAlign: 'left'}} >Hey, <br />I'm Jared</h1>
+                        {/* <p><a style={{width: '100px', marginLeft: '-110px'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV</a></p> */}
+                        <p><a style={{width: '30vw', float:'left'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li style={{backgroundImage: 'url(images/img_bg.jpeg)'}}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner">
+                      <div style={{marginTop: '300px'}} className="desc" >
+                        <h1 style={{textAlign: 'left'}} >I love building<br /> things</h1>
+                        {/* <p><a style={{width: '150px', marginLeft: '-60px'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts" target="_blank" rel="noopener noreferrer">View Projects</a></p> */}
+                        <p><a style={{width: '40vw', float:'left'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts" target="_blank" rel="noopener noreferrer">View Projects</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  )
 }
