e85309139a8c7366f7da703f93f7daac1c038c66
Jared Lim <jaredlim320@gmail.com>
2020-12-22 15:12:16 -0600

    Updates

---
+++ precache-manifest.ac1f1f046d159c29fda08482a6d6067c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97cbf3c15f8b8035da76",
+    "url": "/static/css/main.de3f2d53.chunk.css"
+  },
+  {
+    "revision": "97cbf3c15f8b8035da76",
+    "url": "/static/js/main.97cbf3c1.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ea089c583e222e215502",
+    "url": "/static/js/2.ea089c58.chunk.js"
+  },
+  {
+    "revision": "1846aeb6b5f3fe72e06ee279769ff6a7",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.698d616b2f87697a8d17e419a085135d.js"
+  "/precache-manifest.ac1f1f046d159c29fda08482a6d6067c.js"
 );
 
 workbox.clientsClaim();
