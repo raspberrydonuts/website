c9e13746a3fd44e5e2264b07b8787b7d4f24e29e
Jared Lim <jaredlim320@gmail.com>
2020-12-24 23:20:01 -0600

    update img

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -8,9 +8,13 @@ export default class Introduction extends Component {
 
 function Intro(props) {
   const [width, setWidth] = React.useState(window.innerWidth);
-  if (width < 800) {
+  if (width < 400) {
+    return <TinyIntro />;
+  } 
+  else if (width < 800) {
     return <MobileIntro />;
-  } else {
+  }
+  else {
     return <DesktopIntro />;
   }
 }
@@ -28,8 +32,8 @@ function DesktopIntro(props) {
                   <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                     <div className="slider-text-inner js-fullheight">
                       <div className="desc">
-                        <h1>Hey, <br />I'm Jared</h1>
-                        <p><a className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV</a></p>
+                        <h1 style={{marginTop: '33vh', textShadow: '1px 1px #000000'}}  >Hey, <br />I'm Jared</h1>
+                        <p style={{textShadow: '1px 1px #000000'}}><a className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV</a></p>
                       </div>
                     </div>
                   </div>
@@ -42,9 +46,9 @@ function DesktopIntro(props) {
                 <div className="row">
                   <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                     <div className="slider-text-inner">
-                      <div className="desc" >
-                        <h1>I love building<br /> things</h1>
-                        <p><a className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts" target="_blank" rel="noopener noreferrer">View Projects</a></p>
+                      <div style={{marginTop: '25vh'}} className="desc" >
+                        <h1 style={{marginTop: '33vh', textShadow: '1px 1px #000000'}}  >I love building<br /> things</h1>
+                        <p style={{textShadow: '1px 1px #000000'}}><a className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts" target="_blank" rel="noopener noreferrer">View Projects</a></p>
                       </div>
                     </div>
                   </div>
@@ -71,9 +75,8 @@ function MobileIntro(props) {
                   <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                     <div className="slider-text-inner js-fullheight">
                       <div style={{marginTop: '300px'}} className="desc">
-                        <h1 style={{textAlign: 'left'}} >Hey, <br />I'm Jared</h1>
-                        {/* <p><a style={{width: '100px', marginLeft: '-110px'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV</a></p> */}
-                        <p><a style={{width: '30vw', float:'left'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV</a></p>
+                        <h1 style={{textAlign: 'left', textShadow: '1px 1px #000000'}} >Hey, <br />I'm Jared</h1>
+                        <p style={{textShadow: '1px 1px #000000'}}><a style={{width: '30vw', float:'left'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV</a></p>
                       </div>
                     </div>
                   </div>
@@ -87,9 +90,51 @@ function MobileIntro(props) {
                   <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                     <div className="slider-text-inner">
                       <div style={{marginTop: '300px'}} className="desc" >
-                        <h1 style={{textAlign: 'left'}} >I love building<br /> things</h1>
-                        {/* <p><a style={{width: '150px', marginLeft: '-60px'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts" target="_blank" rel="noopener noreferrer">View Projects</a></p> */}
-                        <p><a style={{width: '40vw', float:'left'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts" target="_blank" rel="noopener noreferrer">View Projects</a></p>
+                        <h1 style={{textAlign: 'left', textShadow: '1px 1px #000000'}} >I love building<br /> things</h1>
+                        <p style={{textShadow: '1px 1px #000000'}}><a style={{width: '40vw', float:'left'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts" target="_blank" rel="noopener noreferrer">View Projects</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+function TinyIntro(props) {
+  return (
+    <div>
+      <section id="colorlib-hero" className="js-fullheight" data-section="home">
+        <div className="flexslider js-fullheight">
+          <ul className="slides">
+            <li style={{backgroundImage: 'url(images/img_bg.jpeg)'}}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner js-fullheight">
+                      <div style={{marginTop: '300px'}} className="desc">
+                        <h1 style={{textAlign: 'left', textShadow: '1px 1px #000000'}} >Hey, <br />I'm Jared</h1>
+                        <p style={{textShadow: '1px 1px #000000'}}><a style={{width: '30vw', float:'left'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts/resume/blob/main/resume.pdf" target="_blank" rel="noopener noreferrer">View CV</a></p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li style={{backgroundImage: 'url(images/img_bg.jpeg)'}}>
+              <div className="overlay" />
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="slider-text-inner">
+                      <div style={{marginTop: '300px'}} className="desc" >
+                        <h1 style={{textAlign: 'left', textShadow: '1px 1px #000000'}} >I love building<br /> things</h1>
+                        <p style={{textShadow: '1px 1px #000000'}}><a style={{width: '45vw', float:'left'}} className="btn btn-primary btn-learn" href="https://github.com/raspberrydonuts" target="_blank" rel="noopener noreferrer">View Projects</a></p>
                       </div>
                     </div>
                   </div>
